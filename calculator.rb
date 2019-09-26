4d6479f6f22b024c758b9cf990bc5cb8504c4cbc
Tavis Weeden <tavisweeden@gmail.com>
2019-09-26 00:51:35 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,5 @@ first_number = 1# Add your variables here
 second_number = 2.5
 sum = first_number + second_number
 difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
